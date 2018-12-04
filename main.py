19efbbd4285826e7157a55d1a71b8ca4ca70ffc5
YongWook <ywha12@gmail.com>
2018-12-04 13:18:37 +0900

    Fix: removed relu of final layer of generator

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from __future__ import print_function, division
 
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Concatenate, Lambda
-from keras.layers import BatchNormalization, Activation, ZeroPadding2D, Add
-from keras.layers.advanced_activations import PReLU, LeakyReLU
+from keras.layers import Input, Dense, Reshape, Flatten
+from keras.layers import BatchNormalization, Activation
+from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import Conv2DTranspose, Conv2D
-from keras.models import Sequential, Model, model_from_json
+from keras.models import Model, model_from_json
 from keras.optimizers import Adam
 from keras.callbacks import TensorBoard
 import datetime
@@ -13,37 +13,35 @@ from utils import DataLoader, time, make_trainable
 import numpy as np
 import os
 
-import keras.backend as K
-
 DEBUG = 0
 
 class DCGAN():
     def __init__(self):
         # Input shape
         self.channels = 3
-        self.img_height = 128
-        self.img_width = 128
-        self.img_shape = (self.img_height, self.img_width, self.channels)
+        self.img_size = 64
         self.latent_dim = 100
         self.time = time()
-        self.dataset_name = 'celebA'
+        self.dataset_name = 'mix_55_'
+        self.learning_rate = 1e-4
 
-        optimizer = Adam(1e-5)
+        optimizer = Adam(self.learning_rate, beta_1=0.5, decay=0.0001)
+
+        self.gf = 64 # filter size of generator's last layer
+        self.df = 64 # filter size of discriminator's first layer
 
         # Configure data loader
         self.data_loader = DataLoader(dataset_name=self.dataset_name,
-                                      img_res=(self.img_height, self.img_width))
+                                      img_res=(self.img_size, self.img_size))
         self.n_data = self.data_loader.get_n_data()
 
-        # Number of filters in the first layer of G and D
-        self.gf = 32
-        self.df = 32
+
 
         self.generator = self.build_generator()
         print("---------------------generator summary----------------------------")
         self.generator.summary()
 
-        self.generator.compile(loss='psnr',
+        self.generator.compile(loss='mse',
                                optimizer=optimizer,
                                metrics=['mse'])
 
@@ -72,29 +70,29 @@ class DCGAN():
         self.combined.compile(loss=['binary_crossentropy'],
                               optimizer=optimizer)
 
-
     def build_generator(self):
         noise = Input(shape=(self.latent_dim,))
 
-        def deconv2d(layer_input, filters=256, kernel_size=(5, 5), strides=(2, 2), bn=True):
+        def deconv2d(layer_input, filters=256, kernel_size=(4, 4), strides=(2, 2), bn_relu=True):
             """Layers used during upsampling"""
-            #u = UpSampling2D(size=2)(layer_input)
+            # u = UpSampling2D(size=2)(layer_input)
             u = Conv2DTranspose(filters, kernel_size=kernel_size, strides=strides, padding='same')(layer_input)
-            if bn:
-                u = BatchNormalization(momentum=0.8)(u)
-            u = Activation('relu')(u)
+            if bn_relu:
+                u = BatchNormalization(momentum=0.9)(u)
+                u = Activation('relu')(u)
             return u
 
-        generator = Dense(4*4*self.gf*16, activation="relu")(noise)
-        generator = Reshape((4, 4, self.gf*16))(generator)
-        generator = BatchNormalization(momentum=0.8)(generator)
-        generator = deconv2d(generator, filters=self.gf*16, kernel_size=(5, 5), strides=(2, 2))
-        generator = deconv2d(generator, filters=self.gf*8, kernel_size=(5, 5), strides=(2, 2))
-        generator = deconv2d(generator, filters=self.gf*4, kernel_size=(5, 5), strides=(2, 2))
-        generator = deconv2d(generator, filters=self.gf*2, kernel_size=(5, 5), strides=(2, 2))
-        generator = deconv2d(generator, filters=self.gf, kernel_size=(5, 5), strides=(2, 2), bn=False)
+        generator = Dense(16 * self.gf * self.img_size // 16 * self.img_size // 16, activation="relu")(noise)
+        generator = Reshape((self.img_size // 16, self.img_size // 16, self.gf * 16))(generator)
+        generator = BatchNormalization()(generator)
+        generator = Activation('relu')(generator)
+        generator = deconv2d(generator, filters=self.gf * 8)
+        generator = deconv2d(generator, filters=self.gf * 4)
+        generator = deconv2d(generator, filters=self.gf * 2)
+        generator = deconv2d(generator, filters=self.gf    )
+        generator = deconv2d(generator, filters=self.channels, kernel_size=(3,3), strides=(1,1), bn_relu=False)
 
-        gen_img = Conv2D(self.channels, kernel_size=3, padding="same", activation='tanh')(generator)
+        gen_img = Activation('tanh')(generator)
 
         return Model(noise, gen_img)
 
@@ -104,31 +102,30 @@ class DCGAN():
             """Discriminator layer"""
             d = Conv2D(filters, kernel_size=3, strides=strides, padding='same')(layer_input)
             if bn:
-                d = BatchNormalization(momentum=0.8)(d)
+                d = BatchNormalization(momentum=0.9)(d)
             d = LeakyReLU(alpha=0.2)(d)
 
             return d
 
         # Input img = generated image
-        d0 = Input(shape=self.img_shape)
+        d0 = Input(shape=(self.img_size, self.img_size, self.channels))
 
-        d = d_block(d0, self.df, strides=1, bn=False)
-        d = d_block(d, self.df, strides=2)
-        d = d_block(d, self.df*2, strides=1)
+        d = d_block(d0, self.df, strides=2, bn=False)
         d = d_block(d, self.df*2, strides=2)
-        d = d_block(d, self.df*4, strides=1)
         d = d_block(d, self.df*4, strides=2)
-        d = d_block(d, self.df*8, strides=1)
         d = d_block(d, self.df*8, strides=2)
 
-        d = Dense(self.df*16)(d)
-        d = LeakyReLU(alpha=0.2)(d)
         d = Flatten()(d)
         validity = Dense(1, activation='sigmoid')(d)
 
         return Model(d0, validity)
 
     def train(self, epochs, batch_size, sample_interval):
+        def named_logs(model, logs):
+            result = {}
+            for l in zip(model.metrics_names, logs):
+                result[l[0]] = l[1]
+            return result
 
         start_time = datetime.datetime.now()
 
@@ -137,10 +134,13 @@ class DCGAN():
 
         max_iter = int(self.n_data/batch_size)
         os.makedirs('./logs/%s' % self.time, exist_ok=True)
+        tensorboard = TensorBoard('./logs/%s' % self.time)
+        tensorboard.set_model(self.generator)
+
         os.makedirs('models/%s' % self.time, exist_ok=True)
         with open('models/%s/%s_architecture.json' % (self.time, 'generator'), 'w') as f:
             f.write(self.generator.to_json())
-        print("\nbatch size : %d | num_data : %d | max iteration : %d \n" % (batch_size, self.n_data, max_iter))
+        print("\nbatch size : %d | num_data : %d | max iteration : %d | time : %s \n" % (batch_size, self.n_data, max_iter, self.time))
         for epoch in range(1, epochs+1):
             for iter in range(max_iter):
                 # ------------------
@@ -157,16 +157,15 @@ class DCGAN():
 
                 make_trainable(self.discriminator, False)
 
-                g_loss = self.combined.train_on_batch([noise], [valid])
+                logs = self.combined.train_on_batch([noise], [valid])
+                tensorboard.on_epoch_end(iter, named_logs(self.combined, [logs]))
 
                 if iter % (sample_interval // 10) == 0:
                     elapsed_time = datetime.datetime.now() - start_time
                     print("epoch:%d | iter : %d / %d | time : %10s | g_loss : %15s | d_loss : %s " %
-                          (epoch, iter, max_iter, elapsed_time, g_loss, d_loss))
+                          (epoch, iter, max_iter, elapsed_time, logs, d_loss))
 
                 if (iter+1) % sample_interval == 0:
-                    tensorboard = TensorBoard('./logs/%s' % self.time)
-                    tensorboard.set_model(self.generator)
                     self.sample_images(epoch, iter+1)
 
             # save weights after every epoch
@@ -196,4 +195,4 @@ if __name__ == '__main__':
         gan.n_data = 50
         gan.train(epochs=2, batch_size=1, sample_interval=10)
     else:
-        gan.train(epochs=100, batch_size=32, sample_interval=400)
+        gan.train(epochs=20, batch_size=64, sample_interval=400)
